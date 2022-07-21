913ea67c5ec347b3093fd16cce6afce4c8d5b7f0
Server <server@server.com>
2022-07-21 07:54:52 -0700

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Route, BrowserRouter, Routes} from "react-router-dom";
-
+import './index.css'
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<div> Working </div>}/>
+                <Route path='/' element={<div className='text-dark'> Working </div>}/>
             </Routes>
         </BrowserRouter>
     );
