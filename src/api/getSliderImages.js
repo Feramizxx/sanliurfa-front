bba569396c64681669ded120f218d8eb6025d2ac
Taleh Taghiyev <talehtaghiyev@Talehs-MacBook-Pro-2.local>
2022-11-01 19:18:01 +0400

    test url

--- src/api/getSliderImages.js
+++ src/api/getSliderImages.js
@@ -3,6 +3,7 @@ import { API_BASE_URL } from './config';
 
 export const getSliderImages = async () => {
     const url = API_BASE_URL + 'slider-images';
+    console.log(url)
     const response = await axios.get(url);
     return response.data;
 }
